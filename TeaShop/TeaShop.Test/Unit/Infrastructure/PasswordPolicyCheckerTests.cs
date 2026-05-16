9dcfd0890178929f109632f0e301ff5ec351c4e4
1201264isep <1201264@isep.ipp.pt>
2026-05-17 00:37:54 +0100

    feat(test)_Test to reference in ASVS

--- TeaShop/TeaShop.Test/Unit/Infrastructure/PasswordPolicyCheckerTests.cs
+++ TeaShop/TeaShop.Test/Unit/Infrastructure/PasswordPolicyCheckerTests.cs
@@ -70,4 +70,19 @@ public class PasswordPolicyCheckerTests
 
         result.Should().BeTrue();
     }
+
+    [Fact]
+    public async Task Is_64_Character_Password_Valid()
+    {
+        _client.IsPwnedPasswordAsync(Arg.Any<string>())
+            .Returns(false);
+
+        var password = "T9$mK2!vQ7#xLp4@Nz8&cRf1*Hy6^uBw3!Jd5%Sa9@Gt2#Vm7&Kq4LJ78H6G4DR1";
+
+        password.Length.Should().Be(64);
+
+        var result = await _checker.IsValidAsync(password);
+
+        result.Should().BeTrue();
+    }
 }
